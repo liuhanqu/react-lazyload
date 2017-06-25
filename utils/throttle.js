2433aa9ee116434cfefc88c08a3416f2f8ce958a
laohan <120936337@qq.com>
2017-06-25 20:53:49 +0800

    modify throttle func

--- utils/throttle.js
+++ utils/throttle.js
@@ -1,28 +1,12 @@
 function throttle(fn, wait) {
   let timer = null;
-  let previous = 0;
   return function(...args) {
-    const now = +new Date();
-    if (!previous) {
-      previous = now;
+    if (timer) {
+      return;
     }
-    const remain = (previous + wait) - now;
-
-    if (remain < 0) {
-      if (timer) {
-        clearTimeout(timer);
-        timer = null;
-      }
-      previous = now;
-      fn.call(this, ...args);
-    } else if (!timer) {
-      timer = setTimeout(function() {
-        timer = null;
-        previous = now;
-        fn.call(this, ...args);
-      }, wait)
-    }
+    timer = setTimeout(function() {
+      timer = null;
+      fn.apply(this, args);
+    }, wait);
   }
 }
-
-export default throttle;
